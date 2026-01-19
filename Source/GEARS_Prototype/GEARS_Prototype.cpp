fda876d0437cfd0d612ed2845e09db21938776d5
M_EtOuais <28m06c02@gmail.com>
2026-01-19 11:33:51 +0100

    :tada: Initial Unreal Engine 5 project structure
    - Create blank C++ project for G.E.A.R.S.

---
+++ Source/GEARS_Prototype/GEARS_Prototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GEARS_Prototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GEARS_Prototype, "GEARS_Prototype" );
---
+++ Source/GEARS_PrototypeEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GEARS_PrototypeEditorTarget : TargetRules
+{
+	public GEARS_PrototypeEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("GEARS_Prototype");
+	}
+}
