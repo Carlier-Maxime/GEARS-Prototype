39d06a6489f7b4760ca0c958124c3a66654a2f75
M_EtOuais <28m06c02@gmail.com>
2026-01-21 21:15:58 +0100

    :sparkles: Add macros for soft pointer validation and handling
    - Introduced `ensureSoftPtr` macro for soft pointer validation logging.
    - Added `ensureSoftPtrOrRet` macro to validate soft pointers with early return on failure.

---
+++ Source/GEARS_Prototype/Public/GEARS_Macro.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#define ensureSoftPtr(SoftPtrVar) \
+ensureMsgf(!(SoftPtrVar).IsNull(), TEXT("Missing [%s] in %s !"), \
+*GET_MEMBER_NAME_CHECKED(ThisClass, SoftPtrVar).ToString(), *GetName())
+
+#define ensureSoftPtrOrRet(SoftPtrVar, Ret) \
+if (!ensureSoftPtr(SoftPtrVar)) return Ret;	
