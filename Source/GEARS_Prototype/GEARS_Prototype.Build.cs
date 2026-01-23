5f432266df40975ff22ba962e74a471d67d98155
M_EtOuais <28m06c02@gmail.com>
2026-01-23 08:44:43 +0100

    :fire: Remove unused comments from Build.cs detailing optional dependencies

--- Source/GEARS_Prototype/GEARS_Prototype.Build.cs
+++ Source/GEARS_Prototype/GEARS_Prototype.Build.cs
@@ -25,13 +25,5 @@ public class GEARS_Prototype : ModuleRules
 			"DeveloperSettings",
 			"AIModule"
 		});
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
