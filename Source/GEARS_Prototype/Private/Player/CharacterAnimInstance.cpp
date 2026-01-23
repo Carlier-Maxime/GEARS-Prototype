582334faf1fd93a2b3adf0af9a1e5f30a2e18a78
M_EtOuais <28m06c02@gmail.com>
2026-01-23 06:13:31 +0100

    :sparkles: Add `CharacterAnimInstance` for optimized animation handling
    - Implement `NativeInitializeAnimation` to cache the Pawn owner.
    - Implement `PreUpdateAnimation` to safely capture character speed on the Game Thread.
    - Implement `NativeThreadSafeUpdateAnimation` for thread-safe PlayRate and state calculations.
    - Add properties for `MovementSpeed`, `MovingPlayRate`, and `AnimReferenceSpeed`.

---
+++ Source/GEARS_Prototype/Private/Player/CharacterAnimInstance.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterAnimInstance.h"
+
+void UCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	Pawn = TryGetPawnOwner();
+}
+
+void UCharacterAnimInstance::PreUpdateAnimation(float DeltaSeconds)
+{
+	Super::PreUpdateAnimation(DeltaSeconds);
+	if (!Pawn) return;
+	Speed = Pawn->GetVelocity().Size2D();
+}
+
+void UCharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	bIsMoving = Speed > 3.f;
+	if (bIsMoving) MovingPlayRate = Speed / AnimReferenceSpeed;
+}
---
+++ Source/GEARS_Prototype/Private/Player/CharacterAnimInstance.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARS_PROTOTYPE_API UCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	virtual void PreUpdateAnimation(float DeltaSeconds) override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Settings")
+	float AnimReferenceSpeed = 300.f;
+
+private:
+	UPROPERTY()
+	TObjectPtr<APawn> Pawn;
+	float Speed;
+	
+	UPROPERTY(BlueprintReadOnly, Category="Movement", meta=(AllowPrivateAccess = true))
+	bool bIsMoving;
+	UPROPERTY(BlueprintReadOnly, Category="Movement", meta=(AllowPrivateAccess = true))
+	float MovingPlayRate;
+};
