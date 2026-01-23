aa899ee9f6750f3c85a1d93f93d1ce830d56d8c6
M_EtOuais <28m06c02@gmail.com>
2026-01-23 16:50:10 +0100

    :sparkles: Add `CameraSettings` with zoom and rotation configuration options
    - Introduced `UCameraSettings` developer settings class.
    - Added configuration for zoom distance, rotation angles, speeds, and axis inversion.
    - Integrated `ZoomDistance` calculations based on `GridSettings`.

---
+++ Source/GEARS_Prototype/Private/Settings/CameraSettings.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CameraSettings.h"
+
+#include "Grid/GridSettings.h"
+
+float UCameraSettings::GetMinZoomDistance() const
+{
+	return GetZoomDistance(MinZoomDistanceFactor);
+}
+
+float UCameraSettings::GetMaxZoomDistance() const
+{
+	return GetZoomDistance(MaxZoomDistanceFactor);
+}
+
+float UCameraSettings::GetDefaultZoomDistance() const
+{
+	return GetZoomDistance(DefaultZoomDistanceFactor);
+}
+
+float UCameraSettings::GetZoomDistance(const float Factor)
+{
+	return Factor * GetDefault<UGridSettings>()->GetCellSize();
+}
---
+++ Source/GEARS_Prototype/Private/Settings/CameraSettings.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "CameraSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config=Game, defaultconfig)
+class GEARS_PROTOTYPE_API UCameraSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation", meta=(
+		ClampMin = -0.01, ClampMax = -90))
+	float MinPitchAngle = -30;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation", meta=(
+		ClampMin = -0.01, ClampMax = -90))
+	float MaxPitchAngle = -70;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation", meta=(
+		ClampMin = -0.01, ClampMax = -90))
+	float DefaultPitchAngle = -50;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation", meta = (
+		UIMin = 0.1, UIMax = 10.0, 
+		ClampMin = 0.01))
+	float RotationSpeed = 1;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	bool bInvertRotationAxis = false;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Zoom", meta=(
+		ClampMin = 0.1, ClampMax = 100, ToolTip="Multiplier based on the grid cell size."))
+	float MinZoomDistanceFactor = 10;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Zoom", meta=(
+		ClampMin = 10, ClampMax = 1000, ToolTip="Multiplier based on the grid cell size."))
+	float MaxZoomDistanceFactor = 100;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Zoom", meta=(
+		ClampMin = 0.1, ClampMax = 1000, ToolTip="Multiplier based on the grid cell size."))
+	float DefaultZoomDistanceFactor = 50;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Zoom", meta = (
+		UIMin = 0.1, UIMax = 10.0, 
+		ClampMin = 0.01))
+	float ZoomSpeed = 1;
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Zoom")
+	bool bInvertZoomAxis = false;
+	
+	UFUNCTION(BlueprintCallable, Category = "Camera")
+	float GetMinZoomDistance() const;
+	UFUNCTION(BlueprintCallable, Category = "Camera")
+	float GetMaxZoomDistance() const;
+	UFUNCTION(BlueprintCallable, Category = "Camera")
+	float GetDefaultZoomDistance() const;
+	UFUNCTION(BlueprintCallable, Category = "Camera")
+	static float GetZoomDistance(float Factor);
+};
