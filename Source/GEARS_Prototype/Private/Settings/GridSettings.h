c9a70b38a1da3cfc5adca4f3aeda9dbfbcf866e6
M_EtOuais <28m06c02@gmail.com>
2026-01-26 10:58:40 +0100

    :sparkles: Add new grid customization parameters
    - Added scalar parameters to support grid cell size factors, transition lengths, and cell count thresholds.
    - Updated `GridSettings` and `DefaultGame.ini` to include new customization options.
    - Modified related assets (`M_Grid.uasset` and `MPC_Grid.uasset`) for compatibility.

--- Source/GEARS_Prototype/Private/Settings/GridSettings.h
+++ Source/GEARS_Prototype/Private/Settings/GridSettings.h
@@ -23,7 +23,12 @@ public:
 	UPROPERTY(EditAnywhere, config, Category = "Grid", meta = (ReadOnlyKeys))
 	TMap<FGameplayTag, float> MPCSharedScalar = {
 		{TAG_Grid_Cell_Size, 100},
-		{TAG_Grid_Border_Thickness, 0.02}
+		{TAG_Grid_Cell_Small_Factor, 0.1},
+		{TAG_Grid_Cell_Big_Factor, 10},
+		{TAG_Grid_Border_Thickness, 0.02},
+		{TAG_Grid_Transition_Length_Factor, 10},
+		{TAG_Grid_Transition_Small_CellCountThreshold, 5},
+		{TAG_Grid_Transition_Big_CellCountThreshold, 100}
 	};
 	
 	UFUNCTION(BlueprintCallable, Category = "Grid")
