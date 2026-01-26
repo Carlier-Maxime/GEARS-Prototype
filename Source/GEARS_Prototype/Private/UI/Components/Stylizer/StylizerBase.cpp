205834ec29143cf02a39c69686ad4b15dfa1f46c
M_EtOuais <28m06c02@gmail.com>
2026-01-26 19:28:04 +0100

    :sparkles: Add `UStylizerBase` class for UI styling logic
    - Introduced `UStylizerBase`, an abstract class for defining UI styling behavior.
    - Added `OnPreConstruct`, `ApplyStyle`, and `GetStyleAsset` methods for reusable style application.
    - Included editor-specific functionality via `PostEditChangeProperty` to update styles during design-time.

---
+++ Source/GEARS_Prototype/Private/UI/Components/Stylizer/StylizerBase.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StylizerBase.h"
+
+void UStylizerBase::OnPreConstruct(bool bIsDesignTime)
+{
+	Super::OnPreConstruct(bIsDesignTime);
+	ApplyStyle();
+}
+
+#if WITH_EDITOR
+void UStylizerBase::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	ApplyStyle();
+}
+#endif
+
+void UStylizerBase::ApplyStyle()
+{
+	ensureMsgf(GetStyleAsset(), TEXT("Style is undefined !"));
+}
---
+++ Source/GEARS_Prototype/Private/UI/Components/Stylizer/StylizerBase.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Extensions/UIComponent.h"
+#include "StylizerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class GEARS_PROTOTYPE_API UStylizerBase : public UUIComponent
+{
+	GENERATED_BODY()
+public:
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+protected:
+	virtual void OnPreConstruct(bool bIsDesignTime) override;
+	virtual const UDataAsset* GetStyleAsset() const PURE_VIRTUAL(UStylizerBase::GetStyleAsset, return nullptr;);
+	template <typename T>
+	T* GetWidget() {
+		if (!GetOwner().IsValid()) return nullptr;
+		const auto Widget = Cast<T>(GetOwner().Get());
+		if (!ensureMsgf(Widget, TEXT("Widget is not of desired type (Maybe is not a good stylizer type)"))) return nullptr;
+		return Widget;
+	}
+	virtual void ApplyStyle();
+};
