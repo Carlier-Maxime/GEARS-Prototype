29bfb447172d7af21a411ceec9ca5262b9089ac7
M_EtOuais <28m06c02@gmail.com>
2026-01-26 19:34:54 +0100

    :sparkles: Add `UBorderStyle` and `UStylizerBorder` for customizable border styling
    - Introduced `UBorderStyle`, a data asset for defining border brush and color properties.
    - Implemented `UStylizerBorder` to apply `UBorderStyle` to `UBorder` widgets.
    - Created new assets `SA_Border_Panel_Main` and `SB_Panel_Main` for border customization.

---
+++ Source/GEARS_Prototype/Private/UI/Components/Stylizer/StylizerBorder.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StylizerBorder.h"
+
+#include "Components/Border.h"
+#include "UI/Styles/BorderStyle.h"
+
+void UStylizerBorder::ApplyStyle()
+{
+	Super::ApplyStyle();
+	const auto Border = GetWidget<UBorder>();
+	if (!Border || !Style || !Style->BrushAsset) return;
+	Border->SetBrush(Style->BrushAsset->Brush);
+	Border->SetBrushColor(Style->BrushColor);
+}
+
+const UDataAsset* UStylizerBorder::GetStyleAsset() const
+{
+	return Style;
+}
---
+++ Source/GEARS_Prototype/Private/UI/Styles/BorderStyle.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Slate/SlateBrushAsset.h"
+#include "BorderStyle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARS_PROTOTYPE_API UBorderStyle : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Style")
+	TObjectPtr<USlateBrushAsset> BrushAsset = nullptr;
+	UPROPERTY(EditDefaultsOnly, Category = "Style")
+	FLinearColor BrushColor = FLinearColor::Gray;
+};
