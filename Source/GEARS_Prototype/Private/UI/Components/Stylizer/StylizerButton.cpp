38fe983e7faeaf57c0fdfd440fc54ce6df9580cb
M_EtOuais <28m06c02@gmail.com>
2026-01-28 17:41:03 +0100

    :sparkles: Add `UStylizerButton` and `UButtonStyleData` for button widget styling
    - Introduced `UButtonStyleData`, a data asset for defining button styles using `FButtonStyle`.
    - Implemented `UStylizerButton` to apply `UButtonStyleData` to `UButton` widgets.
    - Added support for `Slate` style application in `UStylizerButton`.

---
+++ Source/GEARS_Prototype/Private/UI/Components/Stylizer/StylizerButton.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StylizerButton.h"
+
+#include "Components/Button.h"
+
+void UStylizerButton::ApplyStyle()
+{
+	Super::ApplyStyle();
+	const auto Button = GetWidget<UButton>();
+	if (!Button || !Style) return;
+	Button->SetStyle(Style->Slate);
+}
+
+UStyleData* UStylizerButton::GetStyleAsset() const
+{
+	return Style;
+}
---
+++ Source/GEARS_Prototype/Private/UI/Styles/ButtonStyleData.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StyleData.h"
+#include "ButtonStyleData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARS_PROTOTYPE_API UButtonStyleData : public UStyleData
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Style")
+	FButtonStyle Slate;
+	UPROPERTY(EditDefaultsOnly, Category="Style")
+	FLinearColor ColorAndOpacity;
+	UPROPERTY(EditDefaultsOnly, Category="Style")
+	FLinearColor BackgroundColor;
+};
