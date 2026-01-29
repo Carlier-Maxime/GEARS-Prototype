82034d4e92f8cb54effaffdaee92ff6dc9c94745
M_EtOuais <28m06c02@gmail.com>
2026-01-29 12:23:43 +0100

    :recycle: Refine CameraSettings categories for Pitch and Yaw properties
    - Updated `UPROPERTY` categories to more specific `Rotation|Pitch` and `Rotation|Yaw` groups for better organization.

--- Source/GEARS_Prototype/Private/Settings/CameraSettings.h
+++ Source/GEARS_Prototype/Private/Settings/CameraSettings.h
@@ -15,23 +15,24 @@ class GEARS_PROTOTYPE_API UCameraSettings : public UDeveloperSettings
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Pitch")
 	TSoftObjectPtr<UCurveFloat> PitchCurve;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Pitch")
 	float DefaultPitchAngle = -60;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Pitch")
 	bool bInvertPitchAxis = false;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Pitch")
 	bool bLockPitch = false;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Yaw")
 	TSoftObjectPtr<UCurveFloat> YawCurve;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Yaw")
 	float DefaultYawAngle = 0;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Yaw")
 	bool bInvertYawAxis = false;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Yaw")
 	bool bSnapYaw90 = false;
-	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation")
+	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Rotation|Yaw")
 	float SnapYawDuration = 0.5f;
 	
 	UPROPERTY(Config, EditDefaultsOnly, config, Category = "Zoom", meta = (
