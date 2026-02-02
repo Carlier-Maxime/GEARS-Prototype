0d0eeeb0df994118e7fd65d4aea8f52c22831108
M_EtOuais <28m06c02@gmail.com>
2026-02-02 18:31:17 +0100

    :sparkles: Add GridSubsystem with caching and grid-world transformations
    - Introduced `UGridSubsystem` to manage grid-related operations.
    - Added caching for cell size and inverse cell size from `GridSettings`.
    - Provided `WorldToGrid` and `GridToWorld` transformation methods.

---
+++ Source/GEARS_Prototype/Private/Grid/GridSubsystem.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GridSubsystem.h"
+
+#include "Settings/GridSettings.h"
+
+void UGridSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	UpdateCache();
+	const auto Settings = GetMutableDefault<UGridSettings>();
+	Settings->OnUpdated.AddUObject(this, &ThisClass::UpdateCache);
+}
+
+void UGridSubsystem::Deinitialize()
+{
+	const auto Settings = GetMutableDefault<UGridSettings>();
+	Settings->OnUpdated.RemoveAll(this);
+	Super::Deinitialize();
+}
+
+void UGridSubsystem::UpdateCache()
+{
+	const auto Settings = GetDefault<UGridSettings>();
+	CachedCellSize = Settings->GetCellSize();
+	CachedInvCellSize = Settings->GetInvCellSize();
+}
+
+FIntPoint UGridSubsystem::WorldToGrid(const FVector& WorldPosition)
+{
+	return FIntPoint(
+		FMath::FloorToInt(WorldPosition.X * CachedInvCellSize),
+		FMath::FloorToInt(WorldPosition.Y * CachedInvCellSize)
+	);
+}
+
+FVector UGridSubsystem::GridToWorld(const FIntPoint& GridPosition)
+{
+	return FVector(
+		(GridPosition.X + 0.5f) * CachedCellSize,
+		(GridPosition.Y + 0.5f) * CachedCellSize,
+		0.1f
+	);
+}
---
+++ Source/GEARS_Prototype/Private/Grid/GridSubsystem.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "GridSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARS_PROTOTYPE_API UGridSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	FIntPoint WorldToGrid(const FVector& WorldPosition);
+	FVector GridToWorld(const FIntPoint& GridPosition);
+	
+protected:
+	void UpdateCache();
+	
+	private:
+	float CachedCellSize;
+	float CachedInvCellSize;
+};
