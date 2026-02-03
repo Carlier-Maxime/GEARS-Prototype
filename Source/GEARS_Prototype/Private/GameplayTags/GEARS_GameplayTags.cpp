e14a6b94cf5ef8a535c9c68260a5f520ee88744a
M_EtOuais <28m06c02@gmail.com>
2026-02-03 21:22:59 +0100

    :sparkles: Add gameplay tags for resources
    - Introduced new gameplay tags under `resource` categories.
    - Organized tags using `#pragma region` for better code readability.

--- Source/GEARS_Prototype/Private/GameplayTags/GEARS_GameplayTags.cpp
+++ Source/GEARS_Prototype/Private/GameplayTags/GEARS_GameplayTags.cpp
@@ -1,12 +1,27 @@
 ﻿#include "GameplayTags/GEARS_GameplayTags.h"
 
+#pragma region grid
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Cell_Size, "Grid.Cell.Size")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Cell_InvSize, "Grid.Cell.invSize")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Cell_Small_Factor, "Grid.Cell.Small.Factor")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Cell_Big_Factor, "Grid.Cell.Big.Factor")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Cell_Color, "Grid.Cell.Color")
+
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Border_Color, "Grid.Border.Color")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Border_Thickness, "Grid.Border.Thickness")
+
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Transition_Length_Factor, "Grid.Transition.Length.Factor")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Transition_Small_CellCountThreshold, "Grid.Transition.Small.CellCountThreshold")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Grid_Transition_Big_CellCountThreshold, "Grid.Transition.Big.CellCountThreshold")
+#pragma endregion
+
+#pragma region resource
+UE_DEFINE_GAMEPLAY_TAG(TAG_Resource_Organic_Wood, "Resource.Organic.Wood")
+UE_DEFINE_GAMEPLAY_TAG(TAG_Resource_Organic_Biomass, "Resource.Organic.Biomass")
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_Resource_Mineral_Stone, "Resource.Mineral.Stone")
+UE_DEFINE_GAMEPLAY_TAG(TAG_Resource_Mineral_Coal, "Resource.Mineral.Coal")
+UE_DEFINE_GAMEPLAY_TAG(TAG_Resource_Mineral_Iron, "Resource.Mineral.Iron")
+UE_DEFINE_GAMEPLAY_TAG(TAG_Resource_Mineral_Copper, "Resource.Mineral.Copper")
+UE_DEFINE_GAMEPLAY_TAG(TAG_Resource_Mineral_Gold, "Resource.Mineral.Gold")
+#pragma endregion
