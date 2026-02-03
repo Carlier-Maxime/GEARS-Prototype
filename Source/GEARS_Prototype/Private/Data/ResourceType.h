7687d6ef1d7290667619984ae1734580b75b6560
M_EtOuais <28m06c02@gmail.com>
2026-02-03 19:42:36 +0100

    :sparkles: Add `UResourceType` data asset class
    - Introduced `UResourceType` as a `UPrimaryDataAsset` for resource management.
    - Added `ResourceTag`, `WorldMesh`, and noise generation properties (`NoiseThreshold`, `NoiseScale`).

---
+++ Source/GEARS_Prototype/Private/Data/ResourceType.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "ResourceType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARS_PROTOTYPE_API UResourceType : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Identity")
+	FGameplayTag ResourceTag;
+	
+	UPROPERTY(EditAnywhere, Category = "Visuals")
+	TObjectPtr<UStaticMesh> WorldMesh;
+	
+	UPROPERTY(EditAnywhere, Category = "Generation")
+	float NoiseThreshold = 0.6f;
+
+	UPROPERTY(EditAnywhere, Category = "Generation")
+	float NoiseScale = 0.05f;
+};
