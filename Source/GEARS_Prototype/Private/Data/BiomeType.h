5320c2c194c9e1bf6b2ceaa66a3f92281446d54b
M_EtOuais <28m06c02@gmail.com>
2026-02-09 15:09:23 +0100

    :sparkles: Add `UBiomeType` data asset for biome information management
    - Introduced `UBiomeType` class to encapsulate biome-specific data such as tags, temperature, humidity, resources, and visuals.

---
+++ Source/GEARS_Prototype/Private/Data/BiomeType.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "BiomeType.generated.h"
+
+class UResourceType;
+/**
+ * 
+ */
+UCLASS()
+class GEARS_PROTOTYPE_API UBiomeType : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Identity")
+	FGameplayTag BiomeTag;
+
+	UPROPERTY(EditDefaultsOnly, Category="Generation")
+	FFloatRange Temperature;
+	UPROPERTY(EditDefaultsOnly, Category="Generation")
+	FFloatRange Humidity;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Resources")
+	TArray<TSoftObjectPtr<UResourceType>> Resources;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Visuals")
+	FLinearColor Color;
+};
