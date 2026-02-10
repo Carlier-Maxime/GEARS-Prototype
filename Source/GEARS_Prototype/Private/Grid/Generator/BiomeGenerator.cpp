28add4f5264bee7039b9b464fcaf804ef86182bb
M_EtOuais <28m06c02@gmail.com>
2026-02-10 16:00:18 +0100

    :construction: Implement BiomeGenerator foundations
    - Inherit BiomeGenerator from BaseGenerator.
    - Added TemperatureOffset and HumidityOffset to ensure climate noise decorrelation.
    - Introduced SampleBiome method to handle biome identification at a given position.

---
+++ Source/GEARS_Prototype/Private/Grid/Generator/BiomeGenerator.cpp
@@ -0,0 +1,30 @@
+﻿#include "Grid/Generator/BiomeGenerator.h"
+
+#include "Data/BiomeType.h"
+#include "Definitions/GEARS_Macro.h"
+#include "Settings/GridParams.h"
+
+constexpr float RngBiomeMove = 1024.f;
+
+BiomeGenerator::BiomeGenerator(const int32 Seed) :
+		BaseGenerator(Seed),
+		TemperatureOffset(GetRandomOffset({Seed}, RngBiomeMove)),
+		HumidityOffset(GetRandomOffset({Seed}, RngBiomeMove))
+{
+}
+
+int8 BiomeGenerator::SampleBiome(const FGridPosition& Pos) const
+{
+	const auto& Params = GridParams::Get();
+	const float Temp = GetNoiseDensity(Pos, Params.GetTemperature(), TemperatureOffset);
+	const float Humidity = GetNoiseDensity(Pos, Params.GetHumidity(), HumidityOffset);
+	const auto& Registry = Params.GetBiomeRegistry();
+	for (auto i=0; i<Registry.Num(); ++i)
+	{
+		if (!ensureSoftPtr(Registry[i])) continue;
+		const auto Biome = Registry[i].LoadSynchronous();
+		if (!Biome->Temperature.Contains(Temp) || !Biome->Humidity.Contains(Humidity)) continue;
+		return i;
+	}
+	return -1;
+}
---
+++ Source/GEARS_Prototype/Public/Grid/Generator/BiomeGenerator.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "BaseGenerator.h"
+
+struct FBiomeType;
+
+class GEARS_PROTOTYPE_API BiomeGenerator : public BaseGenerator
+{
+public:
+	BiomeGenerator(int32 Seed);
+	
+	int8 SampleBiome(const FGridPosition& Pos) const;
+	
+private:
+	FVector2D TemperatureOffset;
+	FVector2D HumidityOffset;
+};
