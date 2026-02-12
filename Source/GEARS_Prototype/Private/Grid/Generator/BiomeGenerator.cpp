90e5f24bacd9dfe41c76358ced8dff37d73119c4
M_EtOuais <28m06c02@gmail.com>
2026-02-12 03:20:18 +0100

    :building_construction: :recycle: Modularize BiomeGenerator API for external preview
    - Public API: Exposed GetTemperature, GetHumidity, and IsEligible to allow external tools (like Preview Widgets) to query biome state.
    - Refactoring: Cleaned up SampleBiome by delegating logic to these new modular methods.
    - Clarity: Improved readability of the generation pipeline by separating climate sampling from eligibility checks.

--- Source/GEARS_Prototype/Private/Grid/Generator/BiomeGenerator.cpp
+++ Source/GEARS_Prototype/Private/Grid/Generator/BiomeGenerator.cpp
@@ -15,16 +15,35 @@ BiomeGenerator::BiomeGenerator(const int32 Seed) :
 
 int8 BiomeGenerator::SampleBiome(const FGridPosition& Pos) const
 {
-	const auto& Params = GridParams::Get();
-	const float Temp = GetNoiseDensity(Pos, Params.GetTemperature(), TemperatureOffset);
-	const float Humidity = GetNoiseDensity(Pos, Params.GetHumidity(), HumidityOffset);
-	const auto& Registry = Params.GetBiomeRegistry();
+	const float Temp = GetTemperature(Pos);
+	const float Humidity = GetHumidity(Pos);
+	const auto& Registry = GridParams::Get().GetBiomeRegistry();
 	for (auto i=0; i<Registry.Num(); ++i)
 	{
 		if (!ensureSoftPtr(Registry[i])) continue;
 		const auto Biome = Registry[i].LoadSynchronous();
-		if (!Biome->Temperature.Contains(Temp) || !Biome->Humidity.Contains(Humidity)) continue;
+		if (!IsEligible(Temp, Humidity, *Biome)) continue;
 		return i;
 	}
 	return -1;
+}
+
+bool BiomeGenerator::IsEligible(const FGridPosition& Pos, const UBiomeType& Biome) const
+{
+	return IsEligible(GetTemperature(Pos), GetHumidity(Pos), Biome);
+}
+
+bool BiomeGenerator::IsEligible(const float Temp, const float Humidity, const UBiomeType& Biome)
+{
+	return Biome.Temperature.Contains(Temp) && Biome.Humidity.Contains(Humidity);
+}
+
+float BiomeGenerator::GetTemperature(const FGridPosition& Pos) const
+{
+	return GetNoiseDensity(Pos, GridParams::Get().GetTemperature(), TemperatureOffset);
+}
+
+float BiomeGenerator::GetHumidity(const FGridPosition& Pos) const
+{
+	return GetNoiseDensity(Pos, GridParams::Get().GetHumidity(), HumidityOffset);
 }
--- Source/GEARS_Prototype/Public/Grid/Generator/BiomeGenerator.h
+++ Source/GEARS_Prototype/Public/Grid/Generator/BiomeGenerator.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 #include "BaseGenerator.h"
 
-struct FBiomeType;
+class UBiomeType;
 
 class GEARS_PROTOTYPE_API BiomeGenerator : public BaseGenerator
 {
@@ -9,6 +9,10 @@ public:
 	BiomeGenerator(int32 Seed);
 	
 	int8 SampleBiome(const FGridPosition& Pos) const;
+	bool IsEligible(const FGridPosition& Pos, const UBiomeType& Biome) const;
+	static bool IsEligible(float Temp, float Humidity, const UBiomeType& Biome);
+	float GetTemperature(const FGridPosition& Pos) const;
+	float GetHumidity(const FGridPosition& Pos) const;
 	
 private:
 	FVector2D TemperatureOffset;
