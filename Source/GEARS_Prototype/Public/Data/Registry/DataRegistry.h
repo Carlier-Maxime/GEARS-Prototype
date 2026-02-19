c339033c587b2f14687cfdefcba7136c8e899e73
M_EtOuais <28m06c02@gmail.com>
2026-02-19 12:53:01 +0100

    :hammer: Enhance `TDataRegistry` with editor property change handling
    - Integrate editor-specific property change handling to auto-update definitions on asset modification.
    - Reinforce DataWrapper concept to ensure TWrapper is a UObject with StaticClass method.
    - Refactor syntax and structure for better readability and maintainability.

--- Source/GEARS_Prototype/Public/Data/Registry/DataRegistry.h
+++ Source/GEARS_Prototype/Public/Data/Registry/DataRegistry.h
@@ -3,6 +3,8 @@
 
 template<typename T, typename TDef>
 concept DataWrapper = requires(T A) {
+	{ T::StaticClass() } -> std::convertible_to<UClass*>;
+	requires std::derived_from<T, UObject>;
 	{ A.Data } -> std::same_as<TDef&>;
 	{ A.Data.Tag } -> std::same_as<FGameplayTag&>;
 };
@@ -12,47 +14,8 @@ requires DataWrapper<TWrapper, TDefinition>
 class TDataRegistry
 {
 public:
-	TDataRegistry(const FString& RegistryName, const TArray<TSoftObjectPtr<TWrapper>>& InWrappedData)
-	{
-		Definitions.Reserve(InWrappedData.Num());
-		TagToIndex.Reserve(InWrappedData.Num());
-
-		for (auto& Soft : InWrappedData)
-		{
-			if (Definitions.Num() >= std::numeric_limits<TIndexType>::max())
-			{
-				UE_LOG(LogTemp, Fatal, TEXT("[%s] Registry overflow! Increase TIndexType size."), *RegistryName);
-			}
-			
-			TWrapper* LoadedAsset = Soft.LoadSynchronous();
-			if (!LoadedAsset)
-			{
-				UE_LOG(LogTemp, Warning, TEXT("[%s] Failed to load a DataAsset (Null or Invalid path)"), *RegistryName);
-				continue;
-			}
-			
-			auto& Data = LoadedAsset->Data;
-			if (!Data.Tag.IsValid())
-			{
-				UE_LOG(LogTemp, Warning, TEXT("[%s] Asset skipped: Tag is 'None'. Check asset: %s"), 
-					*RegistryName, *LoadedAsset->GetName());
-				continue;
-			}
-			if (TagToIndex.Contains(Data.Tag))
-			{
-				UE_LOG(LogTemp, Error, TEXT("[%s] Duplicate Tag detected: %s"), *RegistryName, *Data.Tag.ToString());
-				continue;
-			}
-
-			const TIndexType Index = static_cast<TIndexType>(Definitions.Add(Data));
-			TagToIndex.Add(Data.Tag, Index);
-          
-			UE_LOG(LogTemp, Verbose, TEXT("[%s] Registered: %s at Index %d"), *RegistryName, *Data.Tag.ToString(), (int32)Index);
-		}
-       
-		UE_LOG(LogTemp, Log, TEXT("[%s] Initialized with %d definitions."), *RegistryName, Definitions.Num());
-	}
-	~TDataRegistry() = default;
+	TDataRegistry(const FString& InRegistryName, const TArray<TSoftObjectPtr<TWrapper>>& InWrappedData);
+	~TDataRegistry();
 	
 	[[nodiscard]] FORCEINLINE const TDefinition& operator[] (const TIndexType Index) const { return Definitions[Index]; }
 	[[nodiscard]] FORCEINLINE int16 GetIndex(const FGameplayTag& Tag) const
@@ -64,7 +27,77 @@ public:
 	[[nodiscard]] FORCEINLINE TIndexType Num() const { return Definitions.Num(); }
 
 	static constexpr auto INVALID_INDEX = static_cast<TIndexType>(-1);
+
 private:
+	FString RegistryName;
 	TArray<TDefinition> Definitions;
 	TMap<FGameplayTag, TIndexType> TagToIndex;
+#if WITH_EDITOR
+	FDelegateHandle OnAnyTWrapperChangedHandle;
+#endif
 };
+
+
+template<typename TDefinition, typename TWrapper, std::integral TIndexType = int32> requires DataWrapper<TWrapper, TDefinition>
+TDataRegistry<TDefinition, TWrapper, TIndexType>::TDataRegistry(const FString& InRegistryName, const TArray<TSoftObjectPtr<TWrapper>>& InWrappedData) : RegistryName(InRegistryName)
+{
+	Definitions.Reserve(InWrappedData.Num());
+	TagToIndex.Reserve(InWrappedData.Num());
+
+	for (auto& Soft : InWrappedData)
+	{
+		if (Definitions.Num() >= std::numeric_limits<TIndexType>::max())
+		{
+			UE_LOG(LogTemp, Fatal, TEXT("[%s] Registry overflow! Increase TIndexType size."), *RegistryName);
+		}
+			
+		TWrapper* LoadedAsset = Soft.LoadSynchronous();
+		if (!LoadedAsset)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("[%s] Failed to load a DataAsset (Null or Invalid path)"), *RegistryName);
+			continue;
+		}
+			
+		auto& Data = LoadedAsset->Data;
+		if (!Data.Tag.IsValid())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("[%s] Asset skipped: Tag is 'None'. Check asset: %s"), 
+				*RegistryName, *LoadedAsset->GetName());
+			continue;
+		}
+		if (TagToIndex.Contains(Data.Tag))
+		{
+			UE_LOG(LogTemp, Error, TEXT("[%s] Duplicate Tag detected: %s"), *RegistryName, *Data.Tag.ToString());
+			continue;
+		}
+
+		const TIndexType Index = static_cast<TIndexType>(Definitions.Add(Data));
+		TagToIndex.Add(Data.Tag, Index);
+          
+		UE_LOG(LogTemp, Verbose, TEXT("[%s] Registered: %s at Index %lld"), *RegistryName, *Data.Tag.ToString(), (int64)Index);
+	}
+       
+	UE_LOG(LogTemp, Log, TEXT("[%s] Initialized with %d definitions."), *RegistryName, Definitions.Num());
+	
+#if WITH_EDITOR
+	OnAnyTWrapperChangedHandle = FCoreUObjectDelegates::OnObjectPropertyChanged.AddLambda([this](UObject* Object, FPropertyChangedEvent& Event)
+	{
+		if (Event.ChangeType == EPropertyChangeType::Interactive) return;
+		if (!Object->IsA(TWrapper::StaticClass())) return;
+		const auto& Data = static_cast<TWrapper*>(Object)->Data;
+		if (!Data.Tag.IsValid()) return;
+		const auto Index = GetIndex(Data.Tag);
+		if (Index == INVALID_INDEX) return;
+		Definitions[Index] = Data;
+		UE_LOG(LogTemp, Verbose, TEXT("[%s] Updated definition of %s at Index %lld"), *RegistryName, *Data.Tag.ToString(), (int64)Index);
+	});
+#endif
+}
+
+template <typename TDefinition, typename TWrapper, std::integral TIndexType> requires DataWrapper<TWrapper, TDefinition>
+TDataRegistry<TDefinition, TWrapper, TIndexType>::~TDataRegistry()
+{
+#if WITH_EDITOR
+	FCoreUObjectDelegates::OnObjectPropertyChanged.Remove(OnAnyTWrapperChangedHandle);
+#endif
+}
