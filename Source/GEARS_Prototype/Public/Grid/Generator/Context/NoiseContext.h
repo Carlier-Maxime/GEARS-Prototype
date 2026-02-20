d7ccdfcae4206e15f982b436decdb084eddb63c8
M_EtOuais <28m06c02@gmail.com>
2026-02-20 18:00:13 +0100

    :lipstick: Add clamping and UI constraints to `NoiseContext` properties
    - Define valid ranges for `Frequency`, `Octaves`, `Persistence`, and `Lacunarity` using `ClampMin`, `ClampMax`, and `UIMax`.
    - Ensure better user input validation and editor UI consistency.

--- Source/GEARS_Prototype/Public/Grid/Generator/Context/NoiseContext.h
+++ Source/GEARS_Prototype/Public/Grid/Generator/Context/NoiseContext.h
@@ -8,15 +8,15 @@ struct FNoiseContext
 {
 	GENERATED_BODY()
 	
-	UPROPERTY(EditAnywhere, Category = "Noise")
+	UPROPERTY(EditAnywhere, Category = "Noise", meta=(ClampMin = 0.00001, UIMax=0.5, ClampMax = 1))
 	float Frequency = 0.05f;
 
-	UPROPERTY(EditAnywhere, Category = "Noise", meta = (ClampMin = 1, ClampMax = 8))
+	UPROPERTY(EditAnywhere, Category = "Noise", meta=(ClampMin = 1, ClampMax = 8))
 	int32 Octaves = 3;
 
-	UPROPERTY(EditAnywhere, Category = "Noise")
+	UPROPERTY(EditAnywhere, Category = "Noise", meta=(ClampMin = 0, ClampMax = 1))
 	float Persistence = 0.5f;
 
-	UPROPERTY(EditAnywhere, Category = "Noise")
+	UPROPERTY(EditAnywhere, Category = "Noise", meta=(ClampMin = 1, UIMax = 4))
 	float Lacunarity = 2.0f;
 };
