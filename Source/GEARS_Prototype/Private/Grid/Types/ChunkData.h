65409b618db7dec5bc5d32b6a0d9175754baf324
M_EtOuais <28m06c02@gmail.com>
2026-03-09 18:44:02 +0100

    :sparkles: Add biome retrieval methods to `FChunkData`
    - Implement methods for retrieving biome indices and definitions using `FWorldGridPos` and `FInChunkPos`.

--- Source/GEARS_Prototype/Private/Grid/Types/ChunkData.h
+++ Source/GEARS_Prototype/Private/Grid/Types/ChunkData.h
@@ -10,6 +10,12 @@ struct FChunkData
 	FChunkData();
 	void SetResource(const FInChunkPos& Pos, int16 ResourceIndex);
 	FORCEINLINE void SetResource(const FWorldGridPos& GridPos, int16 ResourceIndex);
+	
+	FORCEINLINE uint8 GetBiomeIndex(const FWorldGridPos& GridPos) const;
+	FORCEINLINE uint8 GetBiomeIndex(const FInChunkPos& Pos) const;
+	FORCEINLINE const FBiomeDefinition& GetBiome(const FWorldGridPos& GridPos) const;
+	FORCEINLINE const FBiomeDefinition& GetBiome(const FInChunkPos& Pos) const;
+	
 	void SetBiome(const FInChunkPos& Pos, uint8 Biome);
 	FORCEINLINE void SetBiome(const FWorldGridPos& GridPos, uint8 Biome);
 	FORCEINLINE const TArray<uint8>& GetBiomeMap() const {return BiomeMap;}
@@ -26,4 +32,24 @@ void FChunkData::SetResource(const FWorldGridPos& GridPos, const int16 ResourceI
 inline void FChunkData::SetBiome(const FWorldGridPos& GridPos, const uint8 Biome)
 {
 	return SetBiome(GridPos.ToInChunkPos(), Biome);
+}
+
+inline uint8 FChunkData::GetBiomeIndex(const FWorldGridPos& GridPos) const
+{
+	return GetBiomeIndex(GridPos.ToInChunkPos());
+}
+
+inline uint8 FChunkData::GetBiomeIndex(const FInChunkPos& Pos) const
+{
+	return BiomeMap[Pos.Flatten()];
+}
+
+inline const FBiomeDefinition& FChunkData::GetBiome(const FWorldGridPos& GridPos) const
+{
+	return GetBiome(GridPos.ToInChunkPos());
+}
+
+inline const FBiomeDefinition& FChunkData::GetBiome(const FInChunkPos& Pos) const
+{
+	return GridParams::Get().GetBiomeRegistry()[GetBiomeIndex(Pos)];
 }
