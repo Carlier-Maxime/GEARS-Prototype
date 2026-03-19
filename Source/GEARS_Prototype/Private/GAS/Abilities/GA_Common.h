8dffba92cdff99bca519a90fe1a4826caaa99f79
M_EtOuais <28m06c02@gmail.com>
2026-03-19 20:55:20 +0100

    :recycle: Overload termination methods for GAS task compatibility
    - Add UFUNCTION wrappers for EndCancel and EndFinish to allow binding to AbilityTask delegates.
    - Separated logic from default boolean parameters to satisfy dynamic multicast delegate requirements.

--- Source/GEARS_Prototype/Private/GAS/Abilities/GA_Common.h
+++ Source/GEARS_Prototype/Private/GAS/Abilities/GA_Common.h
@@ -15,8 +15,22 @@ class GEARS_PROTOTYPE_API UGA_Common : public UGameplayAbility
 {
 	GENERATED_BODY()
 protected:
-	void EndCancel(bool bReplicateEndAbility = true);
-	void EndFinish(bool bReplicateEndAbility = true);
+	void EndCancel(bool bReplicateEndAbility);
+	UFUNCTION()
+	FORCEINLINE void EndCancel();
+	void EndFinish(bool bReplicateEndAbility);
+	UFUNCTION()
+	FORCEINLINE void EndFinish();
 	void EndAbilitySimple(bool bReplicateEndAbility, bool bWasCancelled);
 	bool TryHitResultFrom(const FGameplayEventData* EventData, FHitResult& OutHit, AWorldRenderer** OutRenderer = nullptr, UAbilitySystemComponent** OutASC = nullptr) const;
 };
+
+inline void UGA_Common::EndCancel()
+{
+	EndCancel(true);
+}
+
+inline void UGA_Common::EndFinish()
+{
+	EndFinish(true);
+}
