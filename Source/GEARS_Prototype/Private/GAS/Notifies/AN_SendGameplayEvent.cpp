47b6932cfc1432f42cc9d8ba8e6522a5bef25116
M_EtOuais <28m06c02@gmail.com>
2026-03-19 21:56:54 +0100

    :sparkles: Add `AN_SendGameplayEvent` AnimNotify for firing gameplay events
    - Introduce `UAN_SendGameplayEvent` AnimNotify to send gameplay events during animations.
    - Implement gameplay tag-based event handling with `EventTag` property for customizable event dispatch.

---
+++ Source/GEARS_Prototype/Private/GAS/Notifies/AN_SendGameplayEvent.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AN_SendGameplayEvent.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+FString UAN_SendGameplayEvent::GetNotifyName_Implementation() const
+{
+	return EventTag == FGameplayTag::EmptyTag ? "SendGameplayEvent" : EventTag.ToString();
+}
+
+void UAN_SendGameplayEvent::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                   const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	if (!MeshComp || EventTag == FGameplayTag::EmptyTag) return;
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner) return;
+	auto* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Owner);
+	if (!ASC) return;
+	FGameplayEventData Payload;
+	Payload.Instigator = Owner;
+	ASC->HandleGameplayEvent(EventTag, &Payload);
+}
