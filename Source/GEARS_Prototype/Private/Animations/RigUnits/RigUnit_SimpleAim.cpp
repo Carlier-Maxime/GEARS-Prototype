6b518bc287532db22ff8f930f9a8dfad574dc58e
M_EtOuais <28m06c02@gmail.com>
2026-03-20 23:15:43 +0100

    :sparkles: Introduce `FRigUnit_SimpleAim` for simplified bone aiming in Control Rig
    - Add `RigUnit_SimpleAim` to handle aiming logic between bones using global transforms.
    - Implement mutual aiming support via the `bMutualAim` property.
    - Include auxiliary methods `CalcBoneAxis` and `Aim` for axis calculation and rotation adjustments.

---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_SimpleAim.cpp
@@ -0,0 +1,33 @@
+﻿#include "RigUnit_SimpleAim.h"
+#include "RigVMCore/RigVMExecuteContext.h"
+
+FRigUnit_SimpleAim_Execute()
+{
+	URigHierarchy* Hierarchy = ExecuteContext.Hierarchy;
+	if (!Hierarchy || !Hierarchy->Contains(Base) || !Hierarchy->Contains(Target)) return;
+	auto BaseTransform = Hierarchy->GetGlobalTransform(Base);
+	auto TargetTransform = Hierarchy->GetGlobalTransform(Target);
+	
+	Aim(*Hierarchy, Base, BaseTransform, TargetTransform);
+	if (!bMutualAim) return;
+	Aim(*Hierarchy, Target, TargetTransform, BaseTransform);
+}
+
+FVector FRigUnit_SimpleAim::CalcBoneAxis(const URigHierarchy& Hierarchy, const FRigElementKey& Bone)
+{
+	TArray<int32> Children = Hierarchy.GetChildren(Hierarchy.GetIndex(Bone));
+	if (Children.Num() == 0) return FVector::ZeroVector;
+	const auto ChildLoc = Hierarchy.GetGlobalTransform(Children[0]).GetLocation();
+	return (ChildLoc - Hierarchy.GetGlobalTransform(Bone).GetLocation()).GetSafeNormal();
+}
+
+void FRigUnit_SimpleAim::Aim(URigHierarchy& Hierarchy, const FRigElementKey& Base, const FTransform& BaseT, const FTransform& TargetT)
+{
+	auto BoneAxis = CalcBoneAxis(Hierarchy, Base);
+	if (!BoneAxis.IsNormalized()) return;
+	auto TargetDir = (TargetT.GetLocation() - BaseT.GetLocation()).GetSafeNormal();
+	FQuat Delta = FQuat::FindBetweenNormals(BoneAxis, TargetDir);
+	FTransform NewBase = BaseT;
+	NewBase.SetRotation(Delta * BaseT.GetRotation());
+	Hierarchy.SetGlobalTransform(Base, NewBase);
+}
---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_SimpleAim.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "Units/RigUnit.h"
+#include "ControlRig.h"
+#include "Math/Axis.h"
+#include "RigUnit_SimpleAim.generated.h"
+
+USTRUCT(meta=(DisplayName="SimpleAim", Category="GEARS|Animation"))
+struct FRigUnit_SimpleAim : public FRigUnitMutable
+{
+	GENERATED_BODY()
+
+	RIGVM_METHOD()
+	GEARS_PROTOTYPE_API virtual void Execute() override;
+
+	UPROPERTY(meta = (Input))
+	FRigElementKey Base = ERigElementType::Bone;
+
+	UPROPERTY(meta = (Input))
+	FRigElementKey Target = ERigElementType::Bone;
+	
+	UPROPERTY(meta = (Input))
+	bool bMutualAim = false;
+	
+private:
+	static FVector CalcBoneAxis(const URigHierarchy& Hierarchy, const FRigElementKey& Bone);
+	static void Aim(URigHierarchy& Hierarchy, const FRigElementKey& Base, const FTransform& BaseT, const FTransform& TargetT);
+};
