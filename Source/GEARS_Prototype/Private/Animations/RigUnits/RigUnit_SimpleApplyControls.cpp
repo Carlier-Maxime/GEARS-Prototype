055756c491838dae05ca24ce178cfa99fcdc88cb
M_EtOuais <28m06c02@gmail.com>
2026-03-23 09:25:42 +0100

    :sparkles: Introduce `FRigUnit_SimpleApplyControls` for propagating control transforms in Control Rig
    - Add `RigUnit_SimpleApplyControls` to transfer global transform data from controllers to driven controls.
    - Implement execution logic to iterate controls and set global transforms on driven keys.

---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_SimpleApplyControls.cpp
@@ -0,0 +1,16 @@
+﻿#include "RigUnit_SimpleApplyControls.h"
+
+FRigUnit_SimpleApplyControls_Execute()
+{
+	URigHierarchy* Hierarchy = ExecuteContext.Hierarchy;
+	if (!Hierarchy) return;
+	
+	for (FRigControlElement* Controller : Hierarchy->GetControls())
+	{
+		auto GlobalTransform = Controller->GetTransform().Current.Global.Get();
+		for (const FRigElementKey& DrivenKey : Controller->Settings.DrivenControls)
+		{
+			Hierarchy->SetGlobalTransform(DrivenKey, GlobalTransform);
+		}
+	}
+}
---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_SimpleApplyControls.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Units/RigUnit.h"
+#include "RigUnit_SimpleApplyControls.generated.h"
+
+USTRUCT(meta=(DisplayName="SimpleApplyControls", Category="GEARS|Animation"))
+struct FRigUnit_SimpleApplyControls : public FRigUnitMutable
+{
+	GENERATED_BODY()
+
+	RIGVM_METHOD()
+	GEARS_PROTOTYPE_API virtual void Execute() override;
+};
