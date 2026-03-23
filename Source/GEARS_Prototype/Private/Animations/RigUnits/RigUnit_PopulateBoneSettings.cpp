4e73583661ea00938a3cd285ce2fa741041e6fe9
M_EtOuais <28m06c02@gmail.com>
2026-03-23 11:38:34 +0100

    :sparkles: Add `FRigUnit_PopulateBoneSettings` for generating bone settings from controls
    - Implement execution logic to populate `FPBIKBoneSetting` based on control-driven bone settings.
    - Add limit validation and error handling for mismatched control limits.
    - Introduce helper method `GetLimitType` to determine limit type for bones.

---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_PopulateBoneSettings.cpp
@@ -0,0 +1,48 @@
+﻿#include "RigUnit_PopulateBoneSettings.h"
+
+FRigUnit_PopulateBoneSettings_Execute()
+{
+	URigHierarchy* Hierarchy = ExecuteContext.Hierarchy;
+	if (!Hierarchy) return;
+	
+	TArray<FRigControlElement*> Controls = Hierarchy->GetControls();
+	
+	Settings.Reset();
+	Settings.Reserve(Controls.Num());
+	for (FRigControlElement* Control : Controls)
+	{
+		TArray<FRigControlLimitEnabled>& Limits = Control->Settings.LimitEnabled;
+		if (!(Limits.Num() == 3 || Limits.Num() == 9))
+		{
+			UE_LOG(LogTemp, Error, TEXT("Mismatch Number of Limits for %s ! (expect: 3 or 9, get: %d)"), *Control->GetName(), Limits.Num());
+			continue;
+		}
+		auto LimitsView = TArrayView<FRigControlLimitEnabled>(Limits).Mid(Limits.Num() == 9 ? 2 : 0, 3);
+		for (const FRigElementKey& DrivenKey : Control->Settings.DrivenControls)
+		{
+			if (DrivenKey.Type != ERigElementType::Bone) continue;
+			FPBIKBoneSetting Setting;
+			Setting.Bone = DrivenKey.Name;
+			Setting.PositionStiffness = 1;
+			Setting.RotationStiffness = 0.2;
+			Setting.X = GetLimitType(LimitsView[0]);
+			Setting.MinX = LimitsView[0].GetForValueType(ERigControlValueType::Minimum);
+			Setting.MaxX = LimitsView[0].GetForValueType(ERigControlValueType::Maximum);
+			Setting.Y = GetLimitType(LimitsView[1]);
+			Setting.MinY = LimitsView[1].GetForValueType(ERigControlValueType::Minimum);
+			Setting.MaxY = LimitsView[1].GetForValueType(ERigControlValueType::Maximum);
+			Setting.Z = GetLimitType(LimitsView[2]);
+			Setting.MinZ = LimitsView[2].GetForValueType(ERigControlValueType::Minimum);
+			Setting.MaxZ = LimitsView[2].GetForValueType(ERigControlValueType::Maximum);
+			Settings.Add(Setting);
+		}
+	}
+}
+
+EPBIKLimitType FRigUnit_PopulateBoneSettings::GetLimitType(const FRigControlLimitEnabled& Limit)
+{
+	if (Limit.IsOff()) return EPBIKLimitType::Free;
+	if (!Limit.bMaximum || !Limit.bMinimum) return EPBIKLimitType::Limited;
+	if (Limit.GetForValueType(ERigControlValueType::Minimum) != Limit.GetForValueType(ERigControlValueType::Maximum)) return EPBIKLimitType::Limited;
+	return EPBIKLimitType::Locked;
+}
---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_PopulateBoneSettings.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Units/RigUnit.h"
+#include "PBIK_Shared.h"
+#include "RigUnit_PopulateBoneSettings.generated.h"
+
+USTRUCT(meta=(DisplayName="Populate BoneSettings from Controls", Category="GEARS|Animation"))
+struct FRigUnit_PopulateBoneSettings : public FRigUnitMutable
+{
+	GENERATED_BODY()
+	
+	RIGVM_METHOD()
+	GEARS_PROTOTYPE_API virtual void Execute() override;
+	
+	UPROPERTY(meta = (Input, Output))
+	TArray<FPBIKBoneSetting> Settings;
+	
+private:
+	static EPBIKLimitType GetLimitType(const FRigControlLimitEnabled& Limit);
+};
