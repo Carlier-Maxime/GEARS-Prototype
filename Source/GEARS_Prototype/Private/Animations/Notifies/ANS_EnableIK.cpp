1a46455071cc0f499ef050f882f4cf49ad3eca96
M_EtOuais <28m06c02@gmail.com>
2026-03-24 13:05:08 +0100

    :sparkles: Add `UANS_EnableIK` for handling IK activation via animation notifies
    - Implement `NotifyBegin`, `NotifyTick`, and `NotifyEnd` for managing IK weights during animations.
    - Add support for `GameplayTag` and `AlphaCurve` to control IK behavior dynamically.

---
+++ Source/GEARS_Prototype/Private/Animations/Notifies/ANS_EnableIK.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ANS_EnableIK.h"
+
+#include "Animations/Interfaces/AnimIKInterface.h"
+
+void UANS_EnableIK::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration,
+                                const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+	auto* AnimInstance = MeshComp->GetAnimInstance();
+	auto* IKInterface = Cast<IAnimIKInterface>(AnimInstance);
+	if (!IKInterface) return;
+	IKSlot = &IKInterface->GetIK(Tag);
+	AnimInstance->GetCurveValueWithDefault(AlphaCurve, 1, IKSlot->Weight);
+}
+
+void UANS_EnableIK::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+	if (IKSlot) MeshComp->GetAnimInstance()->GetCurveValueWithDefault(AlphaCurve, 1, IKSlot->Weight);
+}
+
+void UANS_EnableIK::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+	auto* IKInterface = Cast<IAnimIKInterface>(MeshComp->GetAnimInstance());
+	if (!IKInterface || !IKSlot) return;
+	IKSlot->Weight = 0;
+	IKSlot = nullptr;
+}
---
+++ Source/GEARS_Prototype/Private/Animations/Notifies/ANS_EnableIK.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "Animations/IKSlot.h"
+#include "ANS_EnableIK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEARS_PROTOTYPE_API UANS_EnableIK : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK", meta=(Categories = "IK"))
+	FGameplayTag Tag;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK")
+	FName AlphaCurve;
+	
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration,
+		const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime,
+		const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference) override;
+	
+private:
+	FIKSlot* IKSlot;
+};
