529f6c385dc6e5634266a5f9d30b8664ae4ddd9e
M_EtOuais <28m06c02@gmail.com>
2026-03-24 13:06:01 +0100

    :sparkles: Add `FRigUnit_GetIKSlot` for retrieving IK slot data via gameplay tags
    - Introduce RigUnit `GetIKSlot` to fetch `FIKSlot` using input gameplay tags.
    - Implement execution logic to interface with `IAnimIKInterface` and retrieve corresponding IK data.

---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_GetIKSlot.cpp
@@ -0,0 +1,17 @@
+﻿#include "RigUnit_GetIKSlot.h"
+
+#include "Animations/Interfaces/AnimIKInterface.h"
+
+FRigUnit_GetIKSlot_Execute()
+{
+	const FRigVMExecuteContext& Context = ExecuteContext;
+	auto Component = ExecuteContext.GetMutableOwningComponent();
+	if (!Component) return;
+	USkeletalMeshComponent* SkelMesh = Cast<USkeletalMeshComponent>(Component);
+	if (!SkelMesh) return;
+	UAnimInstance* AnimInstance = SkelMesh->GetAnimInstance();
+	if (!AnimInstance) return;
+	IAnimIKInterface* IKInterface = Cast<IAnimIKInterface>(AnimInstance);
+	if (!IKInterface) return;
+	Slot = IKInterface->GetIK(Tag);
+}
---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_GetIKSlot.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Animations/IKSlot.h"
+#include "Units/RigUnit.h"
+#include "RigUnit_GetIKSlot.generated.h"
+
+USTRUCT(meta=(DisplayName="Get IKSlot", Category="GEARS|Animation"))
+struct FRigUnit_GetIKSlot : public FRigUnit
+{
+	GENERATED_BODY()
+	
+	RIGVM_METHOD()
+	GEARS_PROTOTYPE_API virtual void Execute() override;
+	
+	UPROPERTY(Transient, meta = (Input))
+	FGameplayTag Tag;
+	
+	UPROPERTY(Transient, meta = (Output))
+	FIKSlot Slot;
+};
