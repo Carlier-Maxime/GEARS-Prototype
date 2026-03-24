62d1cf3c70df57f4d28c6a59e8d231ff6593b054
M_EtOuais <28m06c02@gmail.com>
2026-03-24 17:14:41 +0100

    :sparkles: Add `FRigUnit_SyncControlsToBones` for synchronizing controls with driven bones
    - Implement RigUnit to average transforms across driven controls and update control transforms accordingly.
    - Add static method `AverageTransforms` for computing averaged location, rotation, and scale.

---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_SyncControlsToBones.cpp
@@ -0,0 +1,43 @@
+﻿#include "RigUnit_SyncControlsToBones.h"
+
+FRigUnit_SyncControlsToBones_Execute()
+{
+	URigHierarchy* Hierarchy = ExecuteContext.Hierarchy;
+	if (!Hierarchy) return;
+	
+	TArray<FTransform> Transforms;
+	for (FRigControlElement* Controller : Hierarchy->GetControls(true))
+	{
+		Transforms.Reset();
+		Transforms.Reserve(Controller->Settings.DrivenControls.Num());
+		for (const FRigElementKey& DrivenKey : Controller->Settings.DrivenControls)
+		{
+			Transforms.Add(Hierarchy->GetGlobalTransform(DrivenKey));
+		}
+		Hierarchy->SetGlobalTransform(Controller->GetKey(), AverageTransforms(Transforms), false, false);
+	}
+}
+
+FTransform FRigUnit_SyncControlsToBones::AverageTransforms(const TArray<FTransform>& Transforms)
+{
+	if (Transforms.Num() == 0) return FTransform::Identity;
+	if (Transforms.Num() == 1) return Transforms[0];
+
+	auto AvgLocation = FVector::ZeroVector;
+	auto AvgScale = FVector::ZeroVector;
+	FQuat AvgRotation(0.f, 0.f, 0.f, 0.f);
+
+	auto Weight = 1.0f / static_cast<float>(Transforms.Num());
+
+	for (const FTransform& T : Transforms)
+	{
+		AvgLocation += T.GetLocation() * Weight;
+		AvgScale += T.GetScale3D() * Weight;
+		FQuat TargetQ = T.GetRotation();
+		if ((AvgRotation | TargetQ) < 0.f) TargetQ *= -1.f;
+		AvgRotation += TargetQ * Weight;
+	}
+
+	AvgRotation.Normalize();
+	return FTransform(AvgRotation, AvgLocation, AvgScale);
+}
---
+++ Source/GEARS_Prototype/Private/Animations/RigUnits/RigUnit_SyncControlsToBones.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Units/RigUnit.h"
+#include "RigUnit_SyncControlsToBones.generated.h"
+
+USTRUCT(meta=(DisplayName="SyncControlsToBones", Category="GEARS|Animation"))
+struct FRigUnit_SyncControlsToBones : public FRigUnitMutable
+{
+	GENERATED_BODY()
+	
+	RIGVM_METHOD()
+	virtual void Execute() override;
+	
+	static FTransform AverageTransforms(const TArray<FTransform>& Transforms);
+};
