4c05457994536eceaeb42980f4d6de0e383406ee
M_EtOuais <28m06c02@gmail.com>
2026-03-31 21:09:11 +0200

    :sparkles: Add `FInventoryContainer` for inventory stack management
    - Implemented `FInventoryContainer` to handle item stacks with support for adding, removing, and clearing stacks.
    - Added utility methods: `AddStack()`, `RemoveStack()`, `GetStacks()`, `IsEmpty()`, `IsFull()`, and `SetCapacity()`.

---
+++ Source/GEARS_Prototype/Private/Data/Inventory/InventoryContainer.cpp
@@ -0,0 +1,38 @@
+﻿#include "Data/Inventory/InventoryContainer.h"
+
+bool FInventoryContainer::AddStack(FItemStack& Stack)
+{
+	if (!Stack.IsValid()) return false;
+	FItemStack* FreeSlot = nullptr;
+	for (auto& CurrentStack : Stacks)
+	{
+		if (!FreeSlot && CurrentStack.IsEmpty()) FreeSlot = &CurrentStack;
+		if (CurrentStack.ItemID == Stack.ItemID)
+		{
+			Stack.Quantity = CurrentStack.AddQuantity(Stack.Quantity);
+			if (Stack.Quantity == 0) return true;
+		}
+	}
+	if (FreeSlot)
+	{
+		FreeSlot->ItemID = Stack.ItemID;
+		FreeSlot->Quantity = Stack.Quantity;
+		Stack.Quantity = 0;
+		StackCount++;
+		return true;
+	}
+	if (Stacks.Num() >= GetCapacity()) return false;
+	Stacks.Emplace(Stack);
+	Stack.Quantity = 0;
+	StackCount++;
+	return true;
+}
+
+FItemStack FInventoryContainer::RemoveStack(const int32 SlotIndex)
+{
+	if (SlotIndex < 0 || SlotIndex >= Stacks.Num() ) return {};
+	const auto RemovedStack = Stacks[SlotIndex];
+	Stacks[SlotIndex].Quantity = 0;
+	StackCount--;
+	return RemovedStack;
+}
---
+++ Source/GEARS_Prototype/Public/Data/Inventory/InventoryContainer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "ItemStack.h"
+
+class GEARS_PROTOTYPE_API FInventoryContainer
+{
+	
+public:
+	virtual ~FInventoryContainer() = default;
+	FORCEINLINE const TArray<FItemStack>& GetStacks() const { return Stacks; }
+	FORCEINLINE void Clear() { Stacks.Empty(); }
+	FORCEINLINE bool IsEmpty() const { return StackCount == 0; }
+	FORCEINLINE bool IsFull() const { return StackCount >= GetCapacity(); }
+	FORCEINLINE int32 GetStackCount() const { return StackCount; }
+	FORCEINLINE int32 GetCapacity() const { return Capacity; }
+	FORCEINLINE void SetCapacity(const int32 NewCapacity) { Capacity=NewCapacity; Stacks.Reserve(NewCapacity); }
+	bool AddStack(FItemStack& Stack);
+	FItemStack RemoveStack(int32 SlotIndex);
+	
+private:
+	TArray<FItemStack> Stacks;
+	int32 StackCount = 0;
+	int32 Capacity = 0;
+};
